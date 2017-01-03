41d7c44e86d82c21f601ba5f24c3dc0d1f63a6f6
Marco Havermans <marcohavermans+gitkraken@hotmail.nl>
2017-01-03 13:31:28 +0100

    Added doodleSurfaceView and GameThread. Drawing needs to be replaced.

---
+++ app/src/main/java/com/anthony/marco/doodlejump/DoodleSurfaceView.java
@@ -0,0 +1,61 @@
+package com.anthony.marco.doodlejump;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+/**
+ * Created by marco on 3-1-2017.
+ */
+
+public class DoodleSurfaceView extends SurfaceView implements SurfaceHolder.Callback {
+    private SurfaceHolder surfaceHolder;
+    private GameThread gameThread;
+
+    public DoodleSurfaceView(Context context) {
+        super(context);
+        initialize();
+    }
+
+    public DoodleSurfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initialize();
+    }
+
+    public DoodleSurfaceView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initialize();
+    }
+
+
+    private void initialize(){
+        this.gameThread = new GameThread(this);
+        this.gameThread.setRunning(true);
+
+        this.surfaceHolder = getHolder();
+
+        surfaceHolder.addCallback(this);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        gameThread.start();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        // Stop Thread
+        this.gameThread.setRunning(false);
+        try {
+            this.gameThread.join();
+        } catch (InterruptedException e) {
+
+        }
+    }
+}
---
+++ app/src/main/java/com/anthony/marco/doodlejump/GameThread.java
@@ -0,0 +1,51 @@
+package com.anthony.marco.doodlejump;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.Log;
+
+/**
+ * Created by marco on 3-1-2017.
+ */
+
+public class GameThread extends Thread {
+    private final String TAG = "GameThread";
+    private DoodleSurfaceView doodleSurfaceView;
+
+    private boolean isRunning;
+
+    public GameThread(DoodleSurfaceView doodleSurfaceView) {
+        this.doodleSurfaceView = doodleSurfaceView;
+    }
+
+    @Override
+    public void run() {
+        while (isRunning) {
+            Canvas canvas = doodleSurfaceView.getHolder().lockCanvas();
+
+            if (canvas != null) {
+                synchronized (doodleSurfaceView.getHolder()) {
+                    // Draw here using the local canvas variable
+                    Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+                    paint.setColor(Color.YELLOW);
+                    paint.setStyle(Paint.Style.FILL);
+                    canvas.drawCircle(50, 50, 20, paint);
+                }
+
+                doodleSurfaceView.getHolder().unlockCanvasAndPost(canvas);
+            }
+
+            // Sleep
+            try {
+                sleep(1000 / 60);
+            } catch (InterruptedException ex) {
+                Log.e(TAG, ex.getLocalizedMessage());
+            }
+        }
+    }
+
+    public void setRunning(boolean running) {
+        isRunning = running;
+    }
+}
