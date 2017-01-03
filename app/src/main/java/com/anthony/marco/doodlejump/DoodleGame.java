c5a9e588ed916647d9ecba776a85462f69f385e2
Marco Havermans <marcohavermans+gitkraken@hotmail.nl>
2017-01-03 14:38:57 +0100

    Added for loops for the update,handleInput en draw

--- app/src/main/java/com/anthony/marco/doodlejump/DoodleGame.java
+++ app/src/main/java/com/anthony/marco/doodlejump/DoodleGame.java
@@ -2,36 +2,49 @@ package com.anthony.marco.doodlejump;
 
 import android.graphics.Canvas;
 
+import java.util.ArrayList;
+
 /**
  * Created by marco on 3-1-2017.
  */
 
 public class DoodleGame {
-    public DoodleGame(){
+    private ArrayList<Entity> entities;
+
+    public DoodleGame() {
+        entities = new ArrayList<>();
+    }
+
+    public void startGame() {
 
     }
 
-    public void startGame(){
+    public void stopGame() {
 
     }
 
-    public void stopGame(){
+    private void generatePlatforms() {
 
     }
 
-    private Entity[] generatePlatforms(){
-        return  null;
-    }
-
-    public void update(){
+    public void update() {
         // TODO: Update all entities here
+        for (Entity entity : entities) {
+            entity.update();
+        }
     }
 
-    public void handleInput(){
+    public void handleInput() {
         // TODO: Handle input for the entities that require it
+        for (Entity entity : entities) {
+            entity.handleInput();
+        }
     }
 
-    public void draw(Canvas canvas){
+    public void draw(Canvas canvas) {
         // TODO: Draw the entities on the screen
+        for (Entity entity : entities) {
+            entity.draw(canvas);
+        }
     }
 }
