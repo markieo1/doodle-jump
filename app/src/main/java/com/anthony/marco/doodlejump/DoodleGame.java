0f12b73abd3cae17a450d4dda19884f5061bf070
Marco Havermans <marcohavermans+gitkraken@hotmail.nl>
2017-01-03 14:31:08 +0100

    Added DoodleGame class

---
+++ app/src/main/java/com/anthony/marco/doodlejump/DoodleGame.java
@@ -0,0 +1,37 @@
+package com.anthony.marco.doodlejump;
+
+import android.graphics.Canvas;
+
+/**
+ * Created by marco on 3-1-2017.
+ */
+
+public class DoodleGame {
+    public DoodleGame(){
+
+    }
+
+    public void startGame(){
+
+    }
+
+    public void stopGame(){
+
+    }
+
+    private Entity[] generatePlatforms(){
+        return  null;
+    }
+
+    public void update(){
+        // TODO: Update all entities here
+    }
+
+    public void handleInput(){
+        // TODO: Handle input for the entities that require it
+    }
+
+    public void draw(Canvas canvas){
+        // TODO: Draw the entities on the screen
+    }
+}
--- app/src/main/java/com/anthony/marco/doodlejump/GameThread.java
+++ app/src/main/java/com/anthony/marco/doodlejump/GameThread.java
@@ -1,8 +1,6 @@
 package com.anthony.marco.doodlejump;
 
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.util.Log;
 
 /**
@@ -12,11 +10,13 @@ import android.util.Log;
 public class GameThread extends Thread {
     private final String TAG = "GameThread";
     private DoodleSurfaceView doodleSurfaceView;
+    private DoodleGame doodleGame;
 
     private boolean isRunning;
 
     public GameThread(DoodleSurfaceView doodleSurfaceView) {
         this.doodleSurfaceView = doodleSurfaceView;
+        this.doodleGame = new DoodleGame();
     }
 
     @Override
@@ -25,16 +25,15 @@ public class GameThread extends Thread {
             // 1. Handle Input
             // 2. Update
             // 3. Draw
-            
+            doodleGame.handleInput();
+            doodleGame.update();
+
             Canvas canvas = doodleSurfaceView.getHolder().lockCanvas();
 
             if (canvas != null) {
                 synchronized (doodleSurfaceView.getHolder()) {
                     // Draw here using the local canvas variable
-                    Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-                    paint.setColor(Color.YELLOW);
-                    paint.setStyle(Paint.Style.FILL);
-                    canvas.drawCircle(50, 50, 20, paint);
+                    doodleGame.draw(canvas);
                 }
 
                 doodleSurfaceView.getHolder().unlockCanvasAndPost(canvas);
