5f78ff7dd8b7406c8b5d2d4414eb8c309cf6a295
Marco Havermans <marcohavermans+gitkraken@hotmail.nl>
2017-01-05 15:41:52 +0100

    Added flags hiding all system ui.

--- app/src/main/java/com/anthony/marco/doodlejump/MainActivity.java
+++ app/src/main/java/com/anthony/marco/doodlejump/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
 public class MainActivity extends Activity {
@@ -19,6 +20,13 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        getWindow().getDecorView().setSystemUiVisibility(uiOptions);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
         doodleSurfaceView = (DoodleSurfaceView) findViewById(R.id.doodle_surface_view);
